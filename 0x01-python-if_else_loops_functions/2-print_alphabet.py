2697cccbc7084724f4758b0974b3fffc17615ada
thumeka <thumekatembani21@gmail.com>
2023-08-09 14:49:02 -0700

    space

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(letter), end="")
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        print(chr(ord(char) - 32) if 'a' <= char <= 'z' else char, end="")
-    print()
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+            print("{}".format(c), end="")
+        print("")
