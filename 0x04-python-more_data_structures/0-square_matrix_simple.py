7190f631a1abf68e09cd4490086cc4e05669b49c
thumeka <thumekatembani21@gmail.com>
2023-08-16 10:23:03 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,5 @@ def square_matrix_simple(matrix=[]):
         square_row = []
         for value in row:
             square_row.append(value ** 2)
-            square_matrix.append(square_row)
+        square_matrix.append(square_row)
     return square_matrix
