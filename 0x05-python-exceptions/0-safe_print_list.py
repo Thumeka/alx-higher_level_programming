51990242c00332d5aa809f70d4eecb3ef469ae06
thumeka <thumekatembani21@gmail.com>
2023-08-28 07:41:01 -0700

    error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     safe = 0
-    while safe < x:
+    for i in range(x):
         try:
-            print("{}".format(my_list=[safe]), end="")
+            print(f"{my_list=[safe]}", end="")
         except IndexError:
             break
         safe += 1
-    print("")
+    print()
     return safe
