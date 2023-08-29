36eeba0d68a2ba326b1b23c27d55b59472ac8742
thumeka <thumekatembani21@gmail.com>
2023-08-29 10:55:20 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Instantiation private instance attribute"""
+
+        self.__size = size
