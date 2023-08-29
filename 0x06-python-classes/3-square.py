c85d75646d543e38aa7214b68f1b4d97e3615404
thumeka <thumekatembani21@gmail.com>
2023-08-29 11:31:48 -0700

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Represent a square"""
+    size: def __init__(self, size=0):
+        """Instantiation with optional private instance attribute"""
+        if tpe(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """Public instance method"""
+    def area(self):
+        """
+        Calculates area of square
+
+        Returns:
+            area
+        """
+        return (self.__size)**2
