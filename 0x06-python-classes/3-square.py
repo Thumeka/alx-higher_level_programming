cd56412d9a398ebd4108f23d572a9e7ea49be15f
thumeka <thumekatembani21@gmail.com>
2023-08-29 11:44:18 -0700

    pep

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
     """Represent a square"""
     def __init__(self, size=0):
         """Instantiation with optional private instance attribute"""
-        if tpe(size) != int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
