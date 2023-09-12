3e52d597a6c6a3ea5a2126b8b69f7b8d2d7ac6b3
thumeka <thumekatembani21@gmail.com>
2023-09-12 09:43:31 -0700

    7d

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""
-Module 7-save_to_json_file
-
-Contains function that writes Python obj to file using JSON represenation
-"""
+"""Script that adds all args to a list, and then saves them to a file"""
 import sys
 import json
 
+"""Json functions"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Writes Python obj to file using JSON represenation
-    Args:
-        my_obj: python object
-        filename: file
-    """
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+"""Constants"""
+ARGS = sys.argv[1:]
+FILENAME = "add_item.json"
+
+
+def main():
+    """Load file contents"""
+    try:
+        file_content = load_from_json_file(FILENAME)
+    except FileNotFoundError:
+        file_content = []
+
+    """Update content"""
+    file_content += ARGS
+
+    """Save content"""
+    save_to_json_file(file_content, FILENAME)
+
+
+if __name__ == '__main__':
+    main()
