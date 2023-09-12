f5f336d7a36ee80ad16d04f435ea28b2400fabaa
thumeka <thumekatembani21@gmail.com>
2023-09-12 08:56:43 -0700

    4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 4-to_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """Write a function that returns an object JSON representation(string)"""
+    return json.loads(my_str)
