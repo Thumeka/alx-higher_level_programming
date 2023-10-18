880139d9fac5473b9aefc3944f1b954eff9a0d18
thumeka <thumekatembani21@gmail.com>
2023-10-18 11:32:12 -0700

    9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- lists all the cities in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- results must be sorted in ascending order by cities.id
+-- can use only one SELECT statement
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
