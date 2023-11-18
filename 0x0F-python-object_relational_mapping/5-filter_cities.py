772a37feeaa5fd7c84aa41bffd194f87260fa0ba
thumeka <thumekatembani21@gmail.com>
2023-11-18 02:33:35 -0800

    filters

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,21 +9,25 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
     cursor = db.cursor()
-    query = """SELECT cities.id, cities.name, states.name
+    query = """SELECT cities.name
             FROM cities
-            INNER JOIN states ON cities.state_id = states.id
+            INNER JOIN states ON states.id = cities.state_id
             WHERE states.name = %s
             ORDER BY cities.id ASC"""
     state_name = argv[4]
     cursor.execute(query, (state_name,))
+
     for row in cursor.fetchall():
-        print(row)
+        print(row[0])
+
     cursor.close()
     db.close()
