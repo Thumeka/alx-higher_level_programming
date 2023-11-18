b533bb3d41cb0aa4660f4b987d4b36b2065f2616
thumeka <thumekatembani21@gmail.com>
2023-11-18 05:47:28 -0800

    added if

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a script that prints the first State object from the
+database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+
+
+if __name__ == "__main__":
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': sys.argv[1],
+          'password': sys.argv[2],
+          'database': sys.argv[3]}
+    url = URL.create(**db)
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        first_state = session.query(State).order_by(State.id).first()
+        if first_state:
+            print("{}: {}".format(first_state.id, first_state.name))
+        else:
+            print("Nothing")
+    session.close()
