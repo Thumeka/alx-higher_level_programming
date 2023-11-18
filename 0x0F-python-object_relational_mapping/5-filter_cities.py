2c72aee1894175935e88b7ee6dc49462527dfb05
thumeka <thumekatembani21@gmail.com>
2023-11-18 02:45:02 -0800

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,27 +7,18 @@ lists all cities of that state, using the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    cursor = db.cursor()
-    query = """SELECT cities.name
-            FROM cities
-            INNER JOIN states ON states.id = cities.state_id
-            WHERE states.name = %s
-            ORDER BY cities.id ASC"""
-    state_name = sys.argv[4]
-    cursor.execute(query, (state_name,))
-
-    for row in cursor.fetchall():
-        print(row[0])
-
-    cursor.close()
-    db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    states = cur.execute("SELECT cities.id, cities.name, states.name\
+                          FROM cities JOIN states\
+                          ON cities.state_id = states.id\
+                          ORDER BY cities.id")
+    result = cur.fetchall()
+    matches = ""
+    for row in result:
+        if row[2] == sys.argv[4]:
+            matches += row[1] + ', '
+    print(matches.rstrip(", "))
