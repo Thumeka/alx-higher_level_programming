ad788e4cd0403e04a95a7163d842ae8b161c1c5c
thumeka <thumekatembani21@gmail.com>
2023-12-01 12:01:01 -0800

    6+

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,12 @@ a POST request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)"""
 import urllib.request
 import urllib.parse
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {'email': sys.argv[2]}
+    url = argv[1]
+    value = {'email': argv[2]}
     data = urllib.parse.urlencode(value).encode('ascii')
     req = urllib.request.Request(url, data)
     
