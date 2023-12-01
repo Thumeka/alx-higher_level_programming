d76643e3cf2b6e242c4682e2bc9b85b103bd8355
thumeka <thumekatembani21@gmail.com>
2023-12-01 10:09:43 -0800

    url error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,6 +3,16 @@
 a POST request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)"""
 import urllib.request
+import urllib.parse
 import sys
 
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    
+    with urllib.request.urlopen(req) as rep:
+        print(rep.read().decode('utf-8'))
