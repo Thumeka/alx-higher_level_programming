3f47dd5a36fb5d973976dfa92bd7d2e083654cea
thumeka <thumekatembani21@gmail.com>
2023-12-01 10:37:05 -0800

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email, sends
+a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as rep:
+            print(rep.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
